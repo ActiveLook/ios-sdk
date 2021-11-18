debc20c4bb1aef22819c7d626a2a2c413214e202
ActiveLook-Dev <activelook-dev@activelook.net>
2021-11-18 11:08:52 +0100

    Remove useless files

--- Example/Pods/Target Support Files/ActiveLookSDK/ActiveLookSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ActiveLookSDK : NSObject
-@end
-@implementation PodsDummy_ActiveLookSDK
-@end
--- Example/Pods/Target Support Files/Pods-ActiveLookSDK_Tests/Pods-ActiveLookSDK_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ActiveLookSDK_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ActiveLookSDK_Tests
-@end
