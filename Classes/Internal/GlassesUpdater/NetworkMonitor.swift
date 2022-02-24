9b7f9631378df475d940cc1b680b63fa778d49c9
ActiveLook-Dev <activelook-dev@activelook.net>
2022-02-24 08:04:23 +0100

    start implementing network availability monitoring

---
+++ Classes/Internal/GlassesUpdater/NetworkMonitor.swift
@@ -0,0 +1,59 @@
+/*
+
+ Copyright 2021 Microoled
+ Licensed under the Apache License, Version 2.0 (the “License”);
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an “AS IS” BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ */
+
+
+import Network
+
+extension NWInterface.InterfaceType: CaseIterable {
+    public static var allCases: [NWInterface.InterfaceType] = [
+        .other,
+        .wifi,
+        .cellular,
+        .loopback,
+        .wiredEthernet
+    ]
+}
+
+final class NetworkMonitor {
+    static let shared = NetworkMonitor()
+
+    private let queue = DispatchQueue(label: "LCCConnectivityMonitor")
+    private let monitor: NWPathMonitor
+
+    private(set) var isConnected = false
+    private(set) var isExpensive = false
+    private(set) var currentConnectionType: NWInterface.InterfaceType?
+
+    private init() {
+        monitor = NWPathMonitor()
+    }
+
+    func startMonitoring()
+    {
+        dlog(message: "",line: #line, function: #function, file: #fileID)
+
+        monitor.pathUpdateHandler = { [weak self] path in
+            self?.isConnected = path.status != .unsatisfied
+            self?.isExpensive = path.isExpensive
+            self?.currentConnectionType = NWInterface.InterfaceType.allCases.filter { path.usesInterfaceType($0) }.first
+        }
+        monitor.start(queue: queue)
+
+    }
+
+    func stopMonitoring() {
+        monitor.cancel()
+    }
+}
