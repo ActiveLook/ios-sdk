523258e9a614b775643839d8c9923c10b58ee658
Sylvain Romillon <sylvain.romillon@microoled.net>
2022-11-21 15:06:07 +0100

    fix convertDefault() array size

--- Classes/Internal/ImageMDP05.swift
+++ Classes/Internal/ImageMDP05.swift
@@ -25,8 +25,8 @@ internal class ImageMDP05{
         let height = rotatedPixelsArray.count
         let width = rotatedPixelsArray[0].count
         
-        var encodedImg : [[Int]] =  [[]]
-        
+        var encodedImg : [[Int]] =  [[Int]](repeating: [Int](repeating: 0, count: width), count: height)
+
         //reduce to 4bpp
         for y in 0 ..< height {
             for x in 0 ..< width {
