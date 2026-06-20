6974ba50b53675ed3c9478dd59656549b76dc3fa
Gumbraise <vikiclashonyt@gmail.com>
2026-06-20 16:29:27 +0200

    fix: schedule init poll timer on .common run loop mode for watchOS compatibility

--- Sources/Classes/Internal/GlassesInitializer.swift
+++ Sources/Classes/Internal/GlassesInitializer.swift
@@ -148,12 +148,14 @@ internal class GlassesInitializer: NSObject, CBPeripheralDelegate {
                                              CBUUID.ActiveLookCommandsInterfaceService])
 
         // We're 'polling', or checking regularly that we've received all needed information about the glasses
-        initPollTimer = Timer.scheduledTimer(withTimeInterval: initPollInterval, repeats: true) { (timer) in
+        // Added to .common mode so the timer fires on watchOS where the run loop may be in a non-default mode
+        initPollTimer = Timer(timeInterval: initPollInterval, repeats: true) { (timer) in
             if self.isReady() {
                 self.isDone()
                 timer.invalidate()
             }
         }
+        RunLoop.current.add(initPollTimer!, forMode: .common)
 
         // We're failing after an arbitrary timeout duration
         /*initTimeoutTimer = Timer.scheduledTimer(withTimeInterval: initTimeoutDuration, repeats: false) { _ in
